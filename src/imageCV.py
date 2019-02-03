cf43ae2a1a5a5875871f41c90bc3f6b4cb378a0f
AshwinRajesh <ashwinr2003@yahoo.com>
2019-02-03 11:45:43 -0800

    Test threshold

---
+++ src/imageCV.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+
+img = cv2.imread("/Users/ashwinr/Downloads/testartifact.jpg", 100)
+grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(grayscale, 80, 200, cv2.THRESH_BINARY)
+
+cv2.imshow("Hello", thresh)
+cv2.waitKey(0)
