734fac669b903b4db5c87e90ed6159414839ed2d
Eric Hu <eric.zhang.hu.2002@gmail.com>
2019-02-05 23:41:35 -0800

    started coding

--- src/imageCV.py
+++ src/imageCV.py
@@ -1,9 +1,19 @@
 import cv2
 import numpy as np
 
-# more comments
+"""""
+line of code for Ashwin's end, uncomment when coding"
+"""""
+# img = cv2.imread("/Users/ashwinr/Downloads/testartifact.jpg", 100)
 
-img = cv2.imread("/Users/ashwinr/Downloads/testartifact.jpg", 100)
+
+"""""
+line of code for Eric's end, uncomment when coding"
+
+"""""
+# img = cv2.imread("/Users/erichu/Downloads/8d4bb900b3026aac0024badd9ee8d28e--painted-gourds-french-revolution.jpg", 100)
+
+
 grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(grayscale, 80, 200, cv2.THRESH_BINARY)
 
