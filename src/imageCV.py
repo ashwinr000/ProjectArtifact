3fd55ee84c5e962c83c5c3b1f5863b11a5b687bb
Eric Hu <eric.zhang.hu.2002@gmail.com>
2019-02-05 23:14:33 -0800

    testing git again

--- src/imageCV.py
+++ src/imageCV.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+# more comments
+
 img = cv2.imread("/Users/ashwinr/Downloads/testartifact.jpg", 100)
 grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(grayscale, 80, 200, cv2.THRESH_BINARY)
