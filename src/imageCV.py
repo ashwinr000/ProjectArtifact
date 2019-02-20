e76b7d24767fbc641dd4d9eadf7aa433f62bca51
AshwinRajesh <ashwinr2003@yahoo.com>
2019-02-20 09:27:00 -0800

    Basic blurring

--- src/imageCV.py
+++ src/imageCV.py
@@ -5,18 +5,17 @@ import numpy as np
 line of code for Ashwin's end, uncomment when coding"
 * Received on my end *
 """""
-# img = cv2.imread("/Users/ashwinr/Downloads/testartifact.jpg", 100)
-
+'''
+img = cv2.imread("/Users/ashwinr/Downloads/testartifact.jpg", 100)
+grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(grayscale, 80, 200, cv2.THRESH_BINARY)
+blur = cv2.blur(img, (10, 10))
+cv2.imshow("Hello", blur)
+cv2.waitKey(0)
+'''
 
 """""
 line of code for Eric's end, uncomment when coding"
 
 """""
 # img = cv2.imread("/Users/erichu/Downloads/8d4bb900b3026aac0024badd9ee8d28e--painted-gourds-french-revolution.jpg", 100)
-
-
-grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(grayscale, 80, 200, cv2.THRESH_BINARY)
-
-cv2.imshow("Hello", thresh)
-cv2.waitKey(0)
