faf354f01b870c4dcfc2b2d884055aa18d60ecff
Eric Hu <eric.zhang.hu.2002@gmail.com>
2019-02-20 15:03:54 -0800

    eric now using Atom

--- src/imageCV.py
+++ src/imageCV.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
 
-# img = cv2.imread("/Users/ashwinr/Downloads/testartifact.jpg", 100)
 
-img = cv2.imread(
-    "/Users/erichu/Downloads/8d4bb900b3026aac0024badd9ee8d28e--painted-gourds-french-revolution.jpg", 1)
+"""
+This is Ashwin's side of the code
+"""
+# img = cv2.imread("/Users/ashwinr/Downloads/testartifact.jpg", 100)
 grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(grayscale, 80, 200, cv2.THRESH_BINARY)
 blur = cv2.blur(img, (10, 10))
 cv2.imshow("Hello", blur)
 cv2.waitKey(0)
 
-# img = cv2.imread("/Users/erichu/Downloads/8d4bb900b3026aac0024badd9ee8d28e--painted-gourds-french-revolution.jpg", 100)
 
-x = 1
-print (x)
+"""
+This is Eric's side of the code
+"""
+# img = cv2.imread("/Users/erichu/Downloads/8d4bb900b3026aac0024badd9ee8d28e--painted-gourds-french-revolution.jpg", 1)
