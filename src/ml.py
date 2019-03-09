cb06699172492b9aaff3e27c5c4c70ddc3ab220e
AshwinRajesh <ashwinr2003@yahoo.com>
2019-03-08 21:02:13 -0800

    First run of ml script: ~40% accuracy

--- src/ml.py
+++ src/ml.py
@@ -12,27 +12,25 @@ i = 0
 
 civ_labels = ["Australia", "Aztec", "Cherokee", "Chinese", "Egypt", "Indian", "Seminole", "Zulu"]
 
-
 def pullImages(civ):
-    data = []
-    labels = []
-    for filename in os.listdir("/Users/erichu/Desktop/ProjectArtifact/images/" + civ + "/"):
+    j = 0
+    for filename in os.listdir("/Users/ashwinr/Downloads/ProjectArtifact/images/" + civ + "/"):
         if filename.endswith(".jpg"):
             file_name = os.path.join(
-                "/Users/erichu/Desktop/ProjectArtifact/images/" + civ + "/", filename)
-            print(file_name)
+                "/Users/ashwinr/Downloads/ProjectArtifact/images/" + civ + "/", filename)
             img = cv2.imread(file_name, 10)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             arr = np.array(gray)
             arr = arr / 255.0
-            print(len(arr))
-            arr = arr.reshape(500, 500, 1)
-            data.append(arr)
-            labels.append(i)
-    train_data.append(data[:40])
-    test_data.append(data[40:])
-    train_labels.append(labels[:40])
-    test_labels.append(labels[40:])
+            arr = arr.reshape(500, 500)
+            if j < 40:
+                train_labels.append(i)
+                train_data.append(arr)
+            else:
+                test_labels.append(i)
+                test_data.append(arr)
+            global j
+            j += 1
     global i
     i += 1
 
@@ -51,18 +49,19 @@ train_labels = np.array(train_labels)
 test_data = np.array(test_data)
 test_labels = np.array(test_labels)
 
+
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(500, 500, 1)),
+    keras.layers.Flatten(input_shape=(500, 500)),
     keras.layers.Dense(500, activation=tf.nn.relu),
     keras.layers.Dense(8, activation=tf.nn.softmax)
 ])
 
-model.compile(optimizer='adam',
+model.compile(keras.optimizers.Adam(lr=1e-5),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 
-model.fit(train_data, train_labels, epochs=150)
+model.fit(train_data, train_labels, epochs=50)
 
 test_loss, test_acc = model.evaluate(test_data, test_labels)
 
